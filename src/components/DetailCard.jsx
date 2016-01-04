49022b74e15dc8401e848aadff6303b35e8b0f84
Isak <aslundisak@gmail.com>
2016-01-05 00:31:00 +0100

    Basic structure for details page and some picture changes in main.jsx

--- src/components/DetailCard.jsx
+++ src/components/DetailCard.jsx
@@ -3,7 +3,38 @@ var React = require('react');
 
 var DetailCard = React.createClass({
   render: function(){
-    return ();
+    return (
+      <div className="row">
+        <div className="panel panel-default">
+          <div className="panel-body">
+            <div className="col-sm-4">
+              <h4>Weight</h4>
+              <p>6.9kg</p>
+            </div>
+            <div className="col-sm-4">
+              <h4>Height</h4>
+              <p>0.7m</p>
+            </div>
+            <div className="col-sm-4">
+              <h4>Sex</h4>
+              <p>Male / Female</p>
+            </div>
+            <div className="col-sm-4">
+              <h4>Category</h4>
+              <p>Seed</p>
+            </div>
+            <div className="col-sm-4">
+              <h4>Weight</h4>
+              <p>6.9kg</p>
+            </div>
+            <div className="col-sm-4">
+              <h4>Swag</h4>
+              <p>Yolo v.2</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 });
 
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,5 +1,6 @@
 //This a card that shows a image, name, pokedex_number, and type.
 var React = require('react');
+var Link = require('react-router').Link
 
 var thumbStyle = {
   width: "100%",
@@ -15,30 +16,34 @@ var pokemonIdText = {
 var PokemonCard = React.createClass({
   render: function(){
     return (
-      <div id="pokemon-card" className="col-sm-3">
-        <div className="panel panel-default">
-          <div className="row">
-            <div className="col-xs-12">
-              <img src="img/jag.jpeg" style={thumbStyle}/>
-            </div>
-          </div>
-          <div className="panel-body">
-            <div className="row">
-              <div className="col-xs-8">
-                <h4>Pickachu</h4>
-              </div>
-              <div className="col-xs-4">
-                <h4 style={pokemonIdText}><em>#145</em></h4>
+      <Link to={`/details`}>
+        <div id="pokemon-card" className="col-sm-3">
+          <div className="panel panel-default">
+            <div className="panel-heading">
+              <div className="row">
+                <div className="col-xs-12">
+                  <img src="img/bulb.png" style={thumbStyle}/>
+                </div>
               </div>
             </div>
-            <div className="row">
-              <div className="col-xs-6">
-                <p>Water</p>
+            <div className="panel-body">
+              <div className="row">
+                <div className="col-xs-8">
+                  <h4>Pickachu</h4>
+                </div>
+                <div className="col-xs-4">
+                  <h4 style={pokemonIdText}><em>#145</em></h4>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-xs-6">
+                  <p>Water</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </Link>
     );
   }
 });
